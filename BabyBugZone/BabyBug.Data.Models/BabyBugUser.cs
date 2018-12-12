95d838b859c6c65c3e189e8986af76a5cde3605e
Evgeni Dinev <ewgeni.dinew@gmail.com>
2018-12-12 17:39:29 +0200

    --base components

---
+++ BabyBugZone/BabyBug.Data.Models/BabyBugUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace BabyBug.Data.Models
+{
+    public class BabyBugUser : IdentityUser
+    {
+    }
+}
---
+++ BabyBugZone/BabyBug.Data/BabyBugDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BabyBug.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace BabyBugZone.Data
+{
+    public class BabyBugDbContext : IdentityDbContext<BabyBugUser>
+    {
+        public BabyBugDbContext(DbContextOptions<BabyBugDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
